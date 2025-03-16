cec987c3a73005d773286ba254405e322d561720
FreshGandalf-y <belajohanlangner@gmail.com>
2025-03-16 11:41:02 +0100

    new changes

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const WebSocket = require("ws");
 const fs = require("fs");
 const axios = require('axios');
+const os = require("os");
 
 
 const { json } = require("stream/consumers");
+const { cpus } = require("os");
+const { error } = require("console");
  
-const wss = new WebSocket.Server({ port: 8082});
+const wss = new WebSocket.Server({port: 8082});
 
 var lastcommand;
 var usrname;
+var isCpuDataSet = false;
 
 wss.on("connection", ws => {
     console.log("New client connected!");
@@ -19,7 +23,18 @@ wss.on("connection", ws => {
             const data = JSON.parse(message);
             console.log('recived message', data);
 
-            if (data.type === 'getContentDashbord') {
+            if (data.type === 'desktopsContant') {
+                
+                fs.readFile('contantDesktop.json', 'utf-8', (err, fileData) => {
+                    if (err) {
+                        console.error('mistake', err);
+                        ws.send(JSON.stringify({type: 'error', message: 'mistake by loading File'}));
+                    } else {
+                        ws.send(JSON.stringify({type: 'desk-topsContant', html: fileData}));
+                        console.log("send desktop-pictures")
+                    }
+                })
+            } else if (data.type === 'getContentDashbord') {
                 const isAutorized = true;
 
                 if (isAutorized) {
@@ -42,17 +57,61 @@ wss.on("connection", ws => {
                     
                     console.log("data parsed", data)
                     //await axios.post('http://localhost:5002/save', data);
-                    ws.send(JSON.stringify({type: 'chatResponse', data: data}))
+                    ws.send(JSON.stringify({type: 'chatResponse', data}))
     
                 } catch (error) {
                     console.error("mistake:", error);
                 }
+            } else if (data.type === "CpuUsage") {
+                console.log("will send")
+                const cpus = {
+                    cpu1: 1,
+                    cpu2: 1,
+                    cpu3: 1,
+                    cpu4: 1
+                }
 
+                function getCpuUsage() {
+                    const cpus = os.cpus();
+                    let totalIdle = 0, totalTick = 0;
+                    cpus.forEach(cpu => {
+                        for (let type in cpu.times) {
+                            totalTick += cpu.times[type];
+                        }
+                        totalIdle += cpu.times.idle;
+                    });
+                    return { idle: totalIdle / cpus.length, total: totalTick / cpus.length };
+                }
+                    const start = getCpuUsage();
+                    function hhh() {
+                    setTimeout(() => {
+                        const end = getCpuUsage();
+                        const idleDiff = end.idle - start.idle;
+                        const totalDiff = end.total - start.total;
+                        const usage = 100 - (100 * idleDiff / totalDiff);
+                        const ent = console.log(`CPU Usage: ${usage.toFixed(2)}%`);
+                        
+                    }, 1000);
+                    isCpuDataSet = true;
+                    
+                }
+                function setCpus(arg) {
+                    if (isCpuDataSet == true) {
+                        console.log("jj")
+                        cpus.cpu1 = arg;
+                        cpus.cpu2 = arg;
+                        cpus.cpu3 = arg;
+                        cpus.cpu4 = arg;
+                        ws.send(JSON.stringify({type: "CpuUsage", cpus}))
+                    }
+                }
+                
+                
             }
 
-           
+
         } catch (error) {
-            
+            console.error('mistake', error)
         }
         /*try {
             const data = JSON.parse(message);
